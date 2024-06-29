7ff432a9348917a0815dfa757457f105ce23969b
Shivam lavekar <lavekar101@gmail.com>
2024-06-29 10:42:58 +0530

    Add project files.

---
+++ InsuranceApp/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsuranceApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+
+        return View(); 
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ InsuranceApp/Models/Auth.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InsuranceApp.Models
+{
+    public class Auth
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+    }
+
+    public class OtpLog
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Otp { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsUsed { get; set; }
+    }
+}
